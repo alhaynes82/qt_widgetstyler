e2928119289565230942a8fe7f8c5f18caed357d
alhaynes82 <alhaynes82@gmail.com>
2015-11-24 14:15:10 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='qt_widgetstyler',
+      version='0.1',
+      url='https://github.com/alhaynes82/qt_widgetstyler.git',
+      author='alhaynes82',
+      author_email='alhaynes82@gmail.com',
+      license='MIT',
+      packages='qt_widgetstyler',
+      zip_safe=False)
